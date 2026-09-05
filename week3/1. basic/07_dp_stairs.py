3d890f74491adad6b7bed4aee4f1788458d5f0f1
minseongkim275-dotcom <minseongkim275@gmail.com>
2026-09-05 11:46:24 +0900

    점화식을 통한 하향식 closes #39

--- week3/1. basic/07_dp_stairs.py
+++ week3/1. basic/07_dp_stairs.py
@@ -45,15 +45,24 @@ def climb_stairs(n):
     Returns:
         n번째 계단까지 오르는 방법의 수
     """
+
+
+
     # TODO: 특별한 경우 처리
     pass
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
     
-    
-    # TODO: dp 배열 생성 및 초기화
-    pass
-    
-    # TODO: 작은 문제부터 차례로 계산
-    pass
+    dp = [0 for i in range(n+1)]
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
     
     return dp[n]
 
